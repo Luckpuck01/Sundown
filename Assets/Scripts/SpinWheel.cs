1dc4c60b92b25d33c777d269f6e295d724236fe9
Sadrinhooo <sadrabazi82@gmail.com>
2025-04-01 00:45:58 +0200

    wheeeel

--- Assets/Scripts/SpinWheel.cs
+++ Assets/Scripts/SpinWheel.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public class SpinWheel : MonoBehaviour
 {
     [SerializeField] private float spinSpeed;
+    [SerializeField] private float wheelMotionlessThreshold = 0.5f;
 
     private Rigidbody2D rb2D;
     private Vector2 lastTouchPos;
     private bool isDragging;
+    private bool isSpinning;
     
 
     // Start is called before the first frame update
@@ -16,11 +18,44 @@ public class SpinWheel : MonoBehaviour
     {
         rb2D = GetComponent<Rigidbody2D>();
         rb2D.gravityScale = 0;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        print(Input.touches);
+        if (Input.touchCount > 0 && !isSpinning)
+        {
+            print("Touch");
+            Touch touch = Input.GetTouch(0);
+
+            if (touch.phase == TouchPhase.Began)
+            {
+                isDragging = true;
+                lastTouchPos = touch.position;
+            } else if (touch.phase == TouchPhase.Moved && isDragging)
+            {
+                Vector2 delta = touch.position - lastTouchPos;
+                float spinForce = delta.x * spinSpeed;
+                if (Mathf.Abs(spinForce) > 0.1)
+                {
+                    rb2D.AddTorque(spinForce);
+                    isSpinning = true;
+                }
+
+                lastTouchPos = touch.position;
+     
+            } else if (touch.phase == TouchPhase.Ended)
+            {
+                isDragging = false;
+            }
+
+        }
+
+        if (isSpinning && Mathf.Abs(rb2D.angularVelocity) < wheelMotionlessThreshold)
+        {
+            isSpinning = false;
+            rb2D.angularVelocity = 0;
+        }
     }
 }
